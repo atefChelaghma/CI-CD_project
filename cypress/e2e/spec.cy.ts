38afec347b7fdcdcffd59804a42f9f79874663e3
atefChelaghma <atefchelaghma@gmail.com>
2025-11-26 09:04:53 -0300

    feat: cypress and config in deploy

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+    it('passes', () => {
+        cy.visit('/')
+        cy.contains('Vite + React').should('be.visible')
+        cy.get('button').click().should('contain', 'count is 1')
+    })
+})
